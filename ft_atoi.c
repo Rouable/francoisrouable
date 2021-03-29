3ed22908db010e70ab30d72f7cfd6164ed11789b
Francois Rouable <frouable@c4r1p4.42nice.fr>
2021-03-29 12:38:08 +0200

    ft_atoi et maj libft.h

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,35 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frouable <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: frouable <frouable@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 11:51:27 by frouable          #+#    #+#             */
-/*   Updated: 2021/03/23 13:41:30 by frouable         ###   ########.fr       */
+/*   Updated: 2021/03/29 12:35:20 by frouable         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	tempo_atoi(const char *ch, int sign)
+{
+	int	len;
+	int	result;
+
+	result = 0;
+	len = 0;
+	while (ft_isdigit(*ch))
+	{
+		len++;
+		if (len > 13 && sign < 0)
+			return (0);
+		if (len > 13 && sign > 0 )
+			return (-1);
+		result *= 10;
+		result += *ch - '0';
+		ch++;
+	}
+	return (result * sign);
+}
+
 int	ft_atoi(const char *str)
 {
 	int	index;
-	int	negative;
-	unsigned long long int	result;
+	int	sign;
+	int	result;
+	int	len;
 
 	index = 0;
-	negative = 1;
+	sign = 1;
 	result = 0;
+	len = 0;
 	while (str[index] == ' ' || str[index] == '\n' || str[index] == '\t'
 		|| str[index] == '\v' || str[index] == '\f' || str[index] == '\r')
 		index++;
 	if (str[index] == '+' || str[index] == '-')
 		if (str[index++] == '-')
-			negative *= -1;
-	while (ft_isdigit(str[index]))
-	{
-		result *= 10;
-		result += str[index] - '0';
-		index++;
-	}
-	return (result * negative);
+			sign *= -1;
+	return (tempo_atoi(&str[index], sign));
 }
