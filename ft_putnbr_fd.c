2af6825b8e64a69eeaddd89749346fb3cfbca5e9
Rouable <francois.rouable@gmail.com>
2021-04-07 08:40:24 +0200

    les dernieres fonctions

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frouable <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/06 16:45:17 by frouable          #+#    #+#             */
+/*   Updated: 2021/04/06 16:46:07 by frouable         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	number;
+
+	number = n;
+	if (number < 0)
+	{
+		ft_putchar_fd('-', fd);
+		number *= -1;
+	}
+	if (number > 10)
+		ft_putnbr_fd(number / 10, fd);
+	ft_putchar_fd('0' + (number % 10), fd);
+}
